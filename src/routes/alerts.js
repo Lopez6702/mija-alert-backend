adf70cb86f9cf84879144edfe3f754495b739d16
Carlos Lopez <clopezorjuela@gmail.com>
2026-03-12 21:05:46 -0500

    Agregar node-fetch para WhatsApp API

--- src/routes/alerts.js
+++ src/routes/alerts.js
@@ -49,3 +49,36 @@ router.post('/send', requireApiKey, async (req, res) => {
 });
 
 module.exports = router;
+
+const fetch = require("node-fetch");
+
+async function sendWhatsAppMessage(to, message) {
+
+  const phoneNumberId = process.env.META_PHONE_NUMBER_ID;
+  const token = process.env.META_ACCESS_TOKEN;
+
+  const url = `https://graph.facebook.com/v18.0/${phoneNumberId}/messages`;
+
+  const body = {
+    messaging_product: "whatsapp",
+    to: to,
+    type: "text",
+    text: {
+      body: message
+    }
+  };
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${token}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)
+  });
+
+  const data = await response.json();
+  console.log("WhatsApp response:", data);
+
+  return data;
+}
